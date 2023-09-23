69ee67243e52c228a1ae13d0f389717a7d022132
Hùng Khanh <hungkhanh.lof@gmail.com>
2023-09-24 03:30:21 +0700

    Add swagger for product environment

--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -16,18 +16,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.Run();
