c18cff75eda1a1620872c28e13fa25214dd17b2c
Hùng Khanh <hungkhanh.lof@gmail.com>
2023-09-24 20:57:09 +0700

    Add request template

---
+++ src/Application/Handlers/Template/HandlerTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CabinetManagement.Application.Common.Interfaces;
+using CabinetManagement.Application.ViewModels.Template;
+using MediatR;
+
+namespace CabinetManagement.Application.Handlers.Template;
+public class RequestTemplate : IRequest<TemplateViewModel>
+{
+    //Add parameters
+}
+public class HandlerTemplate : IRequestHandler<RequestTemplate, TemplateViewModel>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+    public HandlerTemplate(IApplicationDbContext dbContext, IMapper mapper)
+    {
+        _mapper = mapper;
+        _dbContext = dbContext;
+    }
+    public async Task<TemplateViewModel> Handle(RequestTemplate request, CancellationToken cancellationToken)
+    {
+        //add code handle
+        return new TemplateViewModel();
+    }
+}
---
+++ src/Application/ViewModels/Template/TemplateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabinetManagement.Application.ViewModels.Template;
+public class TemplateViewModel
+{
+}
